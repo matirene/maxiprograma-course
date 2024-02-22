e281adb685802fb942a7fcb9714914cb534fc1c1
Matias Rene <matirene1@hotmail.com>
2024-02-22 19:27:03 -0300

    default-project

---
+++ primer-proyecto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace primer_proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
