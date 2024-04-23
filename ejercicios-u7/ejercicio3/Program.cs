8c52b416578e6db18ee493179b0bff26e9289c01
Matias Rene <matirene1@hotmail.com>
2024-04-23 18:37:32 -0300

    Ejercicios U7 (3- 4)

---
+++ ejercicios-u7/ejercicio3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 3. Hacer un programa que solicite una cadena de caracteres y dos caracteres sueltos (tres ingresos). 
+            El programa deberá generar una cadena donde todas las ocurrencias del primer carácter dado hayan sido reemplazadas por el segundo. 
+            Mostrar el resultado en pantalla. Ejemplo:
+                CADENA FUENTE: “La mar estaba serena"
+                CARÁCTER 1: ‘a’ CARÁCTER 2: ‘i’
+                CADENA RESULTADO: “Li mir estibi sereni" */
+
+            char[] frase = new char[31];
+            char letra, letraActual, letraNueva;
+            int indice;
+
+            // Solicitamos la frase y la guardamos.
+            Console.WriteLine("Ingrese la frase, letra por letra.");
+            letra = char.Parse(Console.ReadLine());
+
+            int countChar = 0;
+
+            while (letra != '.' && countChar < 30)
+            {
+                frase[countChar] = letra;
+                letra = char.Parse(Console.ReadLine());
+                countChar++;  
+            }
+            frase[countChar] = '\0';
+
+            Console.Write("La frase ingresada es: ");
+            indice = 0;
+            while (frase[indice] != '\0')
+            {
+                Console.Write(frase[indice]);
+                indice++;
+            }
+        
+            // Solicitamos las letras.
+            Console.WriteLine(" ");
+            Console.WriteLine("Ingrese la letra a reemplazar:");
+            letraActual = char.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la letra reemplazante:");
+            letraNueva = char.Parse(Console.ReadLine());
+
+            //Modificamos la frase.
+            indice = 0;
+            while (frase[indice] != '\0')
+            {
+                if(frase[indice] == letraActual)
+                    frase[indice] = letraNueva;
+
+                indice++;
+            }
+
+            Console.Write("La nueva frase es: ");
+            indice = 0;
+            while (frase[indice] != '\0')
+            {
+                Console.Write(frase[indice]);
+                indice++;
+            }
+        }
+    }
+}
---
+++ ejercicios-u7/ejercicio4/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 4. Una empresa comercializa 15 tipos de artículos y por cada venta realizada genera un registro con los siguientes datos:
+                - Número de Artículo (1 a 15)
+                - Cantidad Vendida 
+
+                Puede haber varios registros para el mismo artículo y el último se indica con número de artículo igual a 0.
+                Se pide determinar e informar:
+                a) El número de artículo que más se vendió en total.
+                b) Los números de artículos que no registraron ventas.
+                c) Cuantas unidades se vendieron del número de artículo 10. */
+
+                int[] vCantVendida = new int [15];
+
+                int articulo, cantVendida;
+
+                Console.WriteLine ("Ingrese el numero de articulo: ");
+                articulo = int.Parse(Console.ReadLine());
+                Console.WriteLine ("Ingrese la cantidad vendida: ");
+                cantVendida = int.Parse(Console.ReadLine());
+
+                while (articulo != 0)
+                {
+                    vCantVendida[articulo - 1] += cantVendida;
+
+                    Console.WriteLine ("Ingrese el numero de articulo: ");
+                    articulo = int.Parse(Console.ReadLine());
+                    Console.WriteLine ("Ingrese la cantidad vendida: ");
+                    cantVendida = int.Parse(Console.ReadLine());
+                }
+
+
+                // VENTAS DE TODOS LOS ARTICULOS
+                for (int x = 0; x < 15; x++)
+                {
+                    Console.WriteLine ("La cantidad vendida en el articulo " + (x + 1) + " es: " + vCantVendida[x]);
+                    Console.WriteLine ("------------------------------------------------------");
+                }
+
+                // a) ARTICULO CON MAS VENTAS
+                int maxVentas = vCantVendida[0];
+                int artMaxVentas = 1;
+
+                for (int y = 0; y < 15; y++)
+                {
+                    if (vCantVendida[y] > maxVentas)
+                    {
+                        maxVentas = vCantVendida[y];
+                        artMaxVentas = y + 1;
+                    }
+                }
+
+                Console.WriteLine("- El articulo con mas ventas es el " + artMaxVentas + " con " + maxVentas + " articulos vendidos.");
+
+                // b) ARTICULOS SIN VENTAS
+                for (int z = 0; z < 15; z++)
+                {
+                    if (vCantVendida[z] == 0)
+                    {
+                        Console.WriteLine("- El articulo " + (z + 1) + " no registro ventas.");
+                    }
+                }
+                
+                // c) VENTAS DEL ARTICULO 10
+                Console.WriteLine("- La cantidad de ventas del articulo 10 es de " + vCantVendida[9] + " articulos.");
+
+        }
+    }
+}
